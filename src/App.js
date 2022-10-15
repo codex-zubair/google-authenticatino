153db2d9e680d52c46ac15db3392a0ef7e03a9f3
Rocko <codingwithrock@gmail.com>
2022-10-15 21:22:47 +0600

    Added github login system

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, GithubAuthProvider } from "firebase/auth";
 import { app } from './Firebase/Firebase.init';
 import { useState } from 'react';
 
@@ -9,7 +9,8 @@ function App() {
 
 
   const authentication = getAuth(app);
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
+  const gitHubProvider = new GithubAuthProvider();
 
 
   const [user, setUser] = useState(false);
@@ -17,12 +18,18 @@ function App() {
 
 
   const googleSignUp = () => {
-    signInWithPopup(authentication, provider)
+    signInWithPopup(authentication, googleProvider)
       .then(result => { setUser(result.user); console.log(result.user) })
       .catch(error => console.log(error));
-    ;
-
   }
+
+
+  const gitHubSignUp = () => {
+    signInWithPopup(authentication, gitHubProvider)
+      .then(result => { setUser(result.user); console.log(result) })
+      .catch(error => console.log(error));
+  }
+
 
   const signOutFromGoogle = () => {
     signOut(authentication)
@@ -35,16 +42,21 @@ function App() {
   return (
     <div className="App" style={{ marginTop: '20px' }}>
 
-      {user.email ? <div>
-        <button onClick={signOutFromGoogle}>Sing Out From Google </button>
+      {user.uid
+        ? <div>
+          <button onClick={signOutFromGoogle}>Sing Out From Google </button>
 
-        <h1>email: {user.email}</h1>
-        <h1>User Name {user.displayName}</h1>
-     
+          <h1>email: {user.uid
+          }</h1>
+          <h1>User Name {user.displayName}</h1>
 
-      </div> 
+
+        </div>
         :
-        <button style={{ marginTop: '20px' }} onClick={googleSignUp}>Sign Up With Google </button>
+        <>
+          <button onClick={gitHubSignUp}>Sign Up Using Github</button> <br />
+          <button style={{ marginTop: '20px' }} onClick={googleSignUp}>Sign Up With Google </button>
+        </>
       }
     </div>
   );
